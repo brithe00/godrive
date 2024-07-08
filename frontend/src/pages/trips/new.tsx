29a2e3faf16f6cc27b1c4a73f80c84273f386372
Brian <61732049+brithe00@users.noreply.github.com>
2024-07-08 22:19:29 +0200

    finished the create trip page

--- frontend/src/pages/trips/new.tsx
+++ frontend/src/pages/trips/new.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {
   Alert,
@@ -23,33 +23,121 @@ import {
   DialogTitle,
   Select,
   MenuItem,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
 } from "@mui/material";
+
+import LoadingButton from "@mui/lab/LoadingButton";
 
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
+import dayjs from "dayjs";
+import { calculateEndTime } from "@/utils/utils";
+import { useMutation } from "@apollo/client";
+import { CREATE_TRIP } from "@/graphql/mutations/trip";
+
+import { useRouter } from "next/router";
+
 export default function NewTrip() {
+  const router = useRouter();
+
   const [startLocation, setStartLocation] = useState("");
   const [endLocation, setEndLocation] = useState("");
+  const [date, setDate] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [numberOfPassengers, setNumberOfPassengers] = useState(0);
+  const [vehicleType, setVehicleType] = useState("");
+  const [vehicleModel, setVehicleModel] = useState("");
+  const [licensePlateNumber, setLicensePlateNumber] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [estimatedDuration, setEstimatedDuration] = useState(0);
+
+  const [createTrip, { data, loading, error }] = useMutation(CREATE_TRIP);
+
+  useEffect(() => {
+    if (startTime && estimatedDuration) {
+      const calculatedEndTime = calculateEndTime(
+        startTime,
+        Number(estimatedDuration)
+      );
+      setEndTime(calculatedEndTime);
+    }
+  }, [startTime, estimatedDuration]);
+
+  const handleCreateTrip = async (e) => {
+    try {
+      e.preventDefault();
+
+      const { data } = await createTrip({
+        variables: {
+          input: {
+            startLocation,
+            endLocation,
+            date: date.toISOString(),
+            startTime,
+            estimatedDuration,
+            endTime,
+            price,
+            status: "new",
+            numberOfPassengers,
+            vehicleType,
+            vehicleModel,
+            licensePlateNumber,
+            stopLocations: "",
+          },
+        },
+      });
+
+      setTimeout(() => {
+        router.push(`/trips/${data.createTrip.id}`);
+      }, 1000);
+    } catch (error) {
+      console.error("Create trip error:", e);
+    }
+  };
 
   return (
     <>
       <Container component="main" maxWidth="md">
+        {error && (
+          <Alert style={{ marginBottom: "1rem" }} severity="error">
+            Error : {error.message}
+          </Alert>
+        )}
+
+        {data && data.createTrip && (
+          <Alert style={{ marginBottom: "1rem" }} severity="success">
+            Trip successfully created. Redirecting...
+          </Alert>
+        )}
+
         <Card>
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Trip
+              Create a Trip
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Create a new trip.
+              Share your ride and connect with others.
             </Typography>
 
             <Box
               component="form"
               noValidate
-              //onSubmit={}
+              onSubmit={handleCreateTrip}
               sx={{ mt: 3 }}
             >
               <Grid container spacing={2} mt={0.5}>
+                <Grid item xs={12}>
+                  <Typography
+                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                  >
+                    Departure & Destination :
+                  </Typography>
+                </Grid>
+
                 <Grid item xs={12} sm={6}>
                   <TextField
                     required
@@ -61,6 +149,7 @@ export default function NewTrip() {
                     onChange={(e) => setStartLocation(e.target.value)}
                   />
                 </Grid>
+
                 <Grid item xs={12} sm={6}>
                   <TextField
                     required
@@ -71,61 +160,200 @@ export default function NewTrip() {
                     value={endLocation}
                     onChange={(e) => setEndLocation(e.target.value)}
                   />
+                </Grid>
+
+                <Grid item xs={12} sm={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography
+                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                  >
+                    Date :
+                  </Typography>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
                   <DatePicker
+                    required
                     fullWidth
-                    label="Date de naissance"
+                    label="Date"
                     format="DD/MM/YYYY"
                     sx={{ width: "100%" }}
-                    //value={birthdate}
-                    //onChange={(newValue) => setBirthdate(newValue)}
+                    value={date}
+                    onChange={(newValue) => setDate(dayjs(newValue))}
                   />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography
+                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                  >
+                    Start time & duration :
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={4}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="startTime"
+                    label="Start time"
+                    name="startTime"
+                    value={startTime}
+                    onChange={(e) => setStartTime(e.target.value)}
+                    helperText={`format : "08:00"`}
+                  />
+                </Grid>
+
+                <Grid item xs={4}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="estimatedDuration"
+                    label="Estimated duration"
+                    name="estimatedDuration"
+                    value={estimatedDuration || ""}
+                    onChange={(e) =>
+                      setEstimatedDuration(
+                        e.target.value === "" ? 0 : parseInt(e.target.value, 10)
+                      )
+                    }
+                    helperText={`in minutes : 120 (for 2 hours)`}
+                    type="number"
+                  />
+                </Grid>
+
+                <Grid item xs={4}>
+                  <TextField
+                    required
+                    disabled
+                    fullWidth
+                    id="endTime"
+                    label="Generated end time"
+                    name="endTime"
+                    value={endTime}
+                    onChange={(e) => setEndTime(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography
+                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                  >
+                    Price :
+                  </Typography>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
                   <TextField
                     required
                     fullWidth
-                    id="departure"
-                    label="Departure"
-                    name="departure"
-                    value={startLocation}
-                    onChange={(e) => setStartLocation(e.target.value)}
+                    id="price"
+                    label="Price"
+                    name="price"
+                    value={price || ""}
+                    onChange={(e) =>
+                      setPrice(
+                        e.target.value === "" ? 0 : parseFloat(e.target.value)
+                      )
+                    }
+                    type="number"
                   />
                 </Grid>
 
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="destination"
-                    label="Destination"
-                    name="destination"
-                    value={endLocation}
-                    onChange={(e) => setEndLocation(e.target.value)}
-                  />
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography
+                    sx={{ fontWeight: "bold", textDecoration: "underline" }}
+                  >
+                    Vehicle & Passengers :
+                  </Typography>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                  <FormControl fullWidth>
-                    <InputLabel id="number-of-passengers-label">
-                      Number of passengers
-                    </InputLabel>
-                    <Select
-                      labelId="number-of-passengers-label"
-                      id="number-of-passengers"
-                      name="number-of-passengers"
-                      //value={age}
-                      label="Number of passengers"
-                      //onChange={handleChange}
+                  <FormControl>
+                    <RadioGroup
+                      row
+                      name="vehicleType"
+                      value={vehicleType}
+                      onChange={(e) => setVehicleType(e.target.value)}
                     >
-                      <MenuItem value={10}>1</MenuItem>
-                      <MenuItem value={20}>2</MenuItem>
-                      <MenuItem value={30}>3</MenuItem>
-                    </Select>
+                      <FormControlLabel
+                        control={<Radio value="car" />}
+                        label="Car"
+                      />
+                      <FormControlLabel
+                        control={<Radio value="bus" />}
+                        label="Bus"
+                      />
+                    </RadioGroup>
                   </FormControl>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="numberOfPassengers"
+                    label="Number of passengers"
+                    name="numberOfPassengers"
+                    value={numberOfPassengers || ""}
+                    onChange={(e) =>
+                      setNumberOfPassengers(
+                        e.target.value === "" ? 0 : parseInt(e.target.value, 10)
+                      )
+                    }
+                    type="number"
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    id="vehicleModel"
+                    label="Vehicle model"
+                    name="vehicleModel"
+                    value={vehicleModel}
+                    onChange={(e) => setVehicleModel(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    id="licensePlateNumber"
+                    label="License plate number"
+                    name="licensePlateNumber"
+                    value={licensePlateNumber}
+                    onChange={(e) => setLicensePlateNumber(e.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Divider />
+                </Grid>
+
+                <Grid item xs={12} container justifyContent="flex-end">
+                  <LoadingButton
+                    type="submit"
+                    variant="contained"
+                    loading={loading}
+                  >
+                    Create trip
+                  </LoadingButton>
                 </Grid>
               </Grid>
             </Box>
