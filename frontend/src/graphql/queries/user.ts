9cfa84e1f35e55d68601ea95dc1c0758122e572e
Brian <61732049+brithe00@users.noreply.github.com>
2024-07-08 22:59:51 +0200

    added more front queries

--- frontend/src/graphql/queries/user.ts
+++ frontend/src/graphql/queries/user.ts
@@ -16,3 +16,36 @@ export const ME = gql`
     }
   }
 `;
+
+export const GET_USER = gql`
+  query GetUser($getUserByIdId: String!) {
+    getUserById(id: $getUserByIdId) {
+      id
+      email
+      pictureUrl
+      firstname
+      lastname
+      description
+      birthdate
+      reviewsAsTarget {
+        id
+        rating
+        title
+        comment
+        createdAt
+        author {
+          id
+          firstname
+          lastname
+          pictureUrl
+        }
+      }
+      tripsAsDriver {
+        id
+        startLocation
+        endLocation
+        date
+      }
+    }
+  }
+`;
