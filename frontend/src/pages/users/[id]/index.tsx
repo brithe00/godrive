7d74c12173b3b3edcc46f288097865860d2bca5c
Brian <61732049+brithe00@users.noreply.github.com>
2024-07-08 23:00:11 +0200

    created the page for user

--- frontend/src/pages/users/[id]/index.tsx
+++ frontend/src/pages/users/[id]/index.tsx
@@ -3,19 +3,21 @@
 import { useParams } from "next/navigation";
 
 import { useQuery } from "@apollo/client";
-//import { GET_TRIP } from "@/graphql/queries/trip";
+import { GET_USER } from "@/graphql/queries/user";
+
+// f97cb263-60a4-4d50-afed-df7d29d289e4
 
 export default function User() {
   const params = useParams();
 
-  //const { loading, data, error, refetch } = useQuery(GET_TRIP, {
-  //  variables: { tripId: params?.id },
-  // });
+  const { loading, data, error, refetch } = useQuery(GET_USER, {
+    variables: { getUserByIdId: params?.id },
+  });
 
-  //if (loading) return <div>Loading...</div>;
-  //if (error) return <div>Error: {error.message}</div>;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
-  //console.log(data);
+  console.log(data);
 
   return (
     <>
