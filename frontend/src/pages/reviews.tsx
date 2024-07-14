938f4d1f71f07b4bfdd2afa95903a1c547ed3c1d
Brian <61732049+brithe00@users.noreply.github.com>
2024-07-14 17:58:03 +0200

    created the reviews page

--- frontend/src/pages/reviews.tsx
+++ frontend/src/pages/reviews.tsx
@@ -1,89 +1,156 @@
-import { gql, useQuery } from "@apollo/client";
-import Link from "next/link";
+import React from "react";
+import { useQuery, useMutation } from "@apollo/client";
 import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Avatar,
+  Rating,
+  CircularProgress,
   Card,
   CardContent,
-  Typography,
-  Button,
-  Grid,
   Alert,
-  CircularProgress,
+  Box,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useSelector } from "react-redux";
+import { GET_REVIEWS_AS_AUTHOR } from "@/graphql/queries/review";
+import { DELETE_REVIEW } from "@/graphql/mutations/review";
+import { useRouter } from "next/navigation";
 
-const GET_TRIPS = gql`
-  query GetTrips {
-    trips {
-      id
-      date
-      price
-      status
-      startLocation
-      stopLocations
-      endLocation
-      driver
-      createdAt
-      updatedAt
+const ReviewCard = ({ review, handleDelete, router }) => (
+  <Card sx={{ width: "100%", mb: 2 }}>
+    <CardContent sx={{ display: "flex", alignItems: "center" }}>
+      <ListItem>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            alt={`${review.target.firstname} ${review.target.lastname}`}
+            src={review.target.pictureUrl}
+          />
+          <ListItemText
+            sx={{ marginLeft: "1rem" }}
+            primary={review.title}
+            secondary={
+              <>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  color="textPrimary"
+                >
+                  {`${review.target.firstname} ${review.target.lastname}`}
+                </Typography>
+                {` — ${review.comment}`}
+                <br />
+                <Rating
+                  name="read-only"
+                  value={review.rating}
+                  readOnly
+                  size="small"
+                />
+              </>
+            }
+          />
+        </Box>
+
+        <Box>
+          <ListItemSecondaryAction>
+            <IconButton
+              edge="end"
+              aria-label="visit"
+              onClick={() => router.push(`/users/${review.target.id}`)}
+            >
+              <VisibilityIcon />
+            </IconButton>
+
+            <IconButton
+              sx={{ marginLeft: "1rem" }}
+              edge="end"
+              aria-label="delete"
+              onClick={() => handleDelete(review.id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </Box>
+      </ListItem>
+    </CardContent>
+  </Card>
+);
+
+const ReviewsPage = () => {
+  const router = useRouter();
+  const me = useSelector((state) => state.user.currentUser);
+  const userId = me?.id;
+  const { loading, error, data, refetch } = useQuery(GET_REVIEWS_AS_AUTHOR, {
+    variables: { userId },
+  });
+
+  const [
+    deleteReview,
+    {
+      data: dataDeleteReview,
+      loading: loadingDeleteReview,
+      error: errorDeleteReview,
+    },
+  ] = useMutation(DELETE_REVIEW);
+
+  const handleDelete = async (reviewId) => {
+    try {
+      await deleteReview({
+        variables: { reviewId },
+        refetchQueries: [
+          { query: GET_REVIEWS_AS_AUTHOR, variables: { userId } },
+        ],
+      });
+    } catch (error) {
+      console.error("Error deleting review:", error);
     }
-  }
-`;
-
-const TripsPage = () => {
-  const { loading, error, data } = useQuery(GET_TRIPS);
-
-  if (loading) return <Typography>Loading...</Typography>;
-  if (error) return <Typography>Error :(</Typography>;
+  };
 
   return (
-    <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+    <Container maxWidth="md">
       {error && (
         <Alert style={{ marginBottom: "1rem" }} severity="error">
           Error : {error.message}
         </Alert>
       )}
 
+      {errorDeleteReview && (
+        <Alert style={{ marginBottom: "1rem" }} severity="error">
+          Error : {errorDeleteReview.message}
+        </Alert>
+      )}
+
+      {dataDeleteReview && dataDeleteReview.deleteReview && (
+        <Alert severity="success">Review successfully deleted.</Alert>
+      )}
+
       {loading && <CircularProgress />}
 
-      <Typography variant="h1" gutterBottom>
-        Trips
-      </Typography>
-      <Grid container spacing={3}>
-        {data.trips.map((trip: any) => (
-          <Grid item xs={12} sm={6} md={4} key={trip.id}>
-            <Card sx={{ borderRadius: 2, backgroundColor: "#fff" }}>
-              <CardContent>
-                <Typography variant="h6">
-                  Date: {new Date(trip.date).toLocaleDateString()}
-                </Typography>
-                <Typography>Price: {trip.price}</Typography>
-                <Typography>Status: {trip.status}</Typography>
-                <Typography>Start Location: {trip.startLocation}</Typography>
-                <Typography>Stop Locations: {trip.stopLocations}</Typography>
-                <Typography>End Location: {trip.endLocation}</Typography>
-                {/* <Link href={`/update-trip/${trip.id}`} passHref>
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    style={{ marginTop: '10px' }}
-                  >
-                    Modifiez votre trajet
-                  </Button>
-                </Link>
-                <Link href={`/delete-trip/${trip.id}`} passHref>
-                  <Button
-                    variant='outlined'
-                    color='secondary'
-                    style={{ marginTop: '10px', marginLeft: '10px' }}
-                  >
-                    Supprimez votre trajet
-                  </Button>
-                </Link> */}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
+      <Card>
+        <CardContent>
+          <Typography variant="h4" gutterBottom>
+            My Reviews
+          </Typography>
+          <List>
+            {data?.reviewsAsAuthor.map((review) => (
+              <ReviewCard
+                key={review.id}
+                review={review}
+                handleDelete={handleDelete}
+                router={router}
+              />
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
-export default TripsPage;
+export default ReviewsPage;
