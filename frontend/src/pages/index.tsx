c81245d76f0df96ee1504e12626dc2bdb51b9aa7
Brian <61732049+brithe00@users.noreply.github.com>
2024-07-19 19:32:44 +0200

    changed color

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -50,7 +50,7 @@ function Hero() {
           </Box>
           <Box
             component="span"
-            sx={{ color: "orange", textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }}
+            sx={{ color: "blue", textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }}
           >
             {" "}
             STAGING
