1b99076baa400e98de10ddea76c59ebc8e17e3bf
Brian <61732049+brithe00@users.noreply.github.com>
2024-09-13 13:26:53 +0200

    another random test

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -49,7 +49,7 @@ function Hero() {
           </Box>
           <Box
             component="span"
-            sx={{ color: "orange", textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }}
+            sx={{ color: "blue", textShadow: "2px 2px 4px rgba(0,0,0,0.5)" }}
           >
             {" "}
             STAGING
